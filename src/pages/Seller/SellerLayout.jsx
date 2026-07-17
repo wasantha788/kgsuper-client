4bbb140ca1c812378ce20312b7699551692e970b
Wasantha Jayasinghe <wasanthajayasinghe95@gmail.com>
2026-07-18 02:58:37 +0530

    client fix

--- src/pages/Seller/SellerLayout.jsx
+++ src/pages/Seller/SellerLayout.jsx
@@ -32,13 +32,13 @@ const SellerLayout = () => {
     <div className="flex flex-col min-h-screen">
       {/* Top Navbar */}
       <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white sticky top-0 z-10">
-        <Link to="/">
+      
           <img
             src={assets.logo}
             alt="logo"
             className="cursor-pointer w-32 md:w-36"
           />
-        </Link>
+        
 
         <div className="flex items-center gap-5 text-gray-600">
           <p>Hi! Admin</p>
