45a7442c3e058c9e7b7f03c6308bacf2a43b5a40
Wasantha Jayasinghe <wasanthajayasinghe95@gmail.com>
2026-07-19 17:13:33 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import path from 'path' 
 import { fileURLToPath } from 'url'
+
 
 // Necessary for __dirname to work in ESM (Vite's default)
 const __filename = fileURLToPath(import.meta.url);
@@ -10,17 +11,13 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [
-    react(),
+    react(), 
     tailwindcss(),
   ],
   resolve: {
     alias: {
+      // This tells Vite that "@" refers to your "src" directory
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  preview: {
-    port: 4173,              // default preview port
-    host: true,              // allow external access
-    allowedHosts: ['kgsuper-client-production.up.railway.app'] // Render domain allow
-  }
 })
